1f93c9b84cd6552dac8232aa01bb44b0a56f3817
Suleymanguly Malikov <suleymanmalikov03@gmail.com>
2024-07-23 15:13:17 +0200

    added analytics vercel

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <Projects />
       <About />
+      <Analytics />
     </div>
   );
 }
